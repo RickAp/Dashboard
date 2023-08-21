12524bee9f7a64aefb7fae582bcba409fb3050ba
Ricardo Navarro <ricardoapodaca4@gmail.com>
2023-08-21 14:22:23 -0700

    Update Graphic.test.tsx
    Se elimina issue en test

--- src/components/Graphic/Graphic.test.tsx
+++ src/components/Graphic/Graphic.test.tsx
@@ -2,16 +2,8 @@ import React from "react";
 import Graphic from "./Graphic";
 import { render } from '@testing-library/react';
 
-jest.mock('../GraphicData/GraphicData', () => {
-    return () => <div>GraphicData</div>;
-});
-  
-jest.mock('../GraphicText/GraphicText', () => {
-    return () => <div>GraphicText</div>;
-});
+describe('<Graphic/>', () => {
 
-describe('<Form />', () => {
-    
     // Test 1: Verificamos que se renderiza correctamente
     it('renders without crashing', () => {
         render(<Graphic />);
@@ -24,13 +16,5 @@ describe('<Form />', () => {
         'flex desktop:w-[1122px] tablet:w-[625px] h-[546px] mobile:h-[302px] desktop:ml-[30px] tablet:ml-[30px] mt-[30px] border border-[var(--grayscale-divider)] border-1 rounded-lg'
         );
     });
-
-    // Test 3: Verificamos que los componentes hijo se renderizan correctamente
-    it('renders GraphicData and GraphicText components', () => {
-        const { getByText } = render(<Graphic />);
-        expect(getByText('GraphicData')).toBeInTheDocument();
-        expect(getByText('GraphicText')).toBeInTheDocument();
-    });
-
 });
 
